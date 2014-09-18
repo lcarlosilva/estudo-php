d4031d19b006d6c0baadfa21bc7427be6e60c2d4
Luiz Carlos <luizvaldeco@gmail.com>
2014-09-18 00:09:15 -0300

    bollean
    criação do boolean.php que diz respeitos aos exemplos do livro.
    pag.26

--- com/luiz/variaveis.php
+++ com/luiz/variaveis.php
@@ -4,5 +4,43 @@
 	$sobrenome = "da Silva";
 	
 	echo"Deu certo -> $sobrenome, $nome";
+	echo '<br>';
+	print_r(gettype($sobrenome));
+	
+	//-- variaveis variante --
+	
+	// difine o nome da variavel
+	$variavel = 'nome';
+	
+	// cria variavel identificada pelo conteúdo de $variavel
+	$$variavel = 'maria';
 
+	// exibe variavel $nome na tela
+	echo '<br>';
+	echo $nome; //resultado = maria
+	
+	echo '<br>';
+	//-- Mudanças de valores --
+	
+	$a = 5;
+	$b = $a;
+	$b = 10;
+	
+	echo $a;
+	echo '<br>';
+	echo $b;
+	
+	echo '<br>';
+	//-- variaveis gemeas --
+	
+	print_r(gettype($a));
+	echo '<br>';
+	$a = 5;
+	$b = &$a;
+	$b = 11;
+	
+	echo $a;
+	echo '<br>';
+	echo $b;
+	
 ?>
