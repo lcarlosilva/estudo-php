53ec8c45e0f05ad8e6064b64072e9872f8af38f9
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2014-11-06 13:33:28 -0200

    commit arquivos estudo

---
+++ com/luiz/booleano.php
@@ -0,0 +1,19 @@
+<?php
+
+	// declara variavel com valor TRUE
+	$exibir_nome = TRUE;
+	
+	// teste se exibir nome é TRUE
+	if($exibir_nome){
+		print_r(gettype($exibir_nome) ? 'true <br>':'false <br>');
+		echo 'José da silva';
+	}	
+	
+	//--
+	
+	//declara variavel numerica
+	$umidade = 91;
+	
+	$vai_chover = ($umi);
+
+?>
